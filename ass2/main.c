440857f69f1bcf40f426dccfe188fa2e75f0f9da
Tim Lucca <tim.lucca88@gmail.com>
2019-06-18 23:08:28 -0600

    waits now

--- ass2/main.c
+++ ass2/main.c
@@ -10,7 +10,6 @@
 #include <signal.h>
 #include <sys/msg.h>
 
-volatile sig_atomic_t loop = 3;
 
 #define handle(p) ({ p; int errtmp = errno; if (errtmp != 0)  { perror(#p) ; exit(errtmp); } }) /* From Luke & Dr. Beaty */
 
@@ -19,17 +18,14 @@ void handler(int sigval){
     if(sigval == SIGUSR1){
         char str[] = "SIGUSR1 signal recieved\n";
         write(1, str, sizeof(str));
-        loop--;
     }
     else if(sigval == SIGUSR2){
         char str[] = "SIGUSR2 signal recieved\n";
         write(1, str, sizeof(str));
-        loop--;
     }
     else if(sigval == SIGWINCH){
         char str[] = "SIGWINCH signal recieved\n";
         write(1, str, sizeof(str));
-        loop--;
     }
     return;
 }
@@ -37,6 +33,7 @@ void handler(int sigval){
 int main(){
     
     pid_t fork_pid;
+    int status, exit_status;
 
     handle(fork_pid=fork());
 
@@ -45,15 +42,20 @@ int main(){
     action.sa_handler = handler;
     sigemptyset (&action.sa_mask);
     action.sa_flags = SA_RESTART;
-    sigaction(SIGUSR1, &action, NULL);
-    sigaction(SIGUSR2, &action, NULL);
-    sigaction(SIGWINCH, &action, NULL);
+    handle(sigaction(SIGUSR1, &action, NULL));
+    handle(sigaction(SIGUSR2, &action, NULL));
+    handle(sigaction(SIGWINCH, &action, NULL));
 
     if(fork_pid==0){
         handle(execl("./child", "child", NULL));
     }
     
-    while(loop){}
+    handle(fork_pid=wait(&status));
+
+    if(WIFEXITED(status)){
+        exit_status = WEXITSTATUS(status);
+        assert(exit_status == 0);
+    }
 
     exit(0);
 }
